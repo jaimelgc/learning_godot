2120e22b373757dbfe4ee0e45f196bd5bb4a6a61
jaimelgc <jaimeleongarcia123@gmail.com>
2026-04-07 14:43:59 +0100

    save

---
+++ space-shooter/scenes/level.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_meteor_timer_timeout() -> void:
+	pass # Replace with function body.
---
+++ space-shooter/scenes/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed = 250
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity += direction * speed
+	move_and_slide()
