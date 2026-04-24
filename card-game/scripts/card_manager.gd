3ffab191dcdee708f5ad751ed460912c4b4ff733
jaimelgc <jaimeleongarcia123@gmail.com>
2026-04-24 14:06:41 +0100

    save

--- card-game/scripts/card_manager.gd
+++ card-game/scripts/card_manager.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 var card_being_dragged
+var is_hovering_on_card
+var screen_size
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
 func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_position = get_global_mouse_position()
-		card_being_dragged.position = mouse_position
+		card_being_dragged.position = Vector2(clamp(mouse_position.x, 0, screen_size.x), clamp(mouse_position.y, 0, screen_size.y))
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -16,7 +20,25 @@ func _input(event):
 				card_being_dragged = card
 		else:
 			card_being_dragged = null
-			
+		
+func connect_card_signals(card):
+	card.connect('hovered', on_hovered_over_card)
+	card.connect('hovered_out', on_hovered_out_card)
+	
+func on_hovered_over_card(card):
+	highlight_card(card, true)
+	
+func on_hovered_out_card(card):
+	highlight_card(card, false)
+	
+func highlight_card(card, hovered):
+	if hovered:
+		card.scale = Vector2(1.05, 1.05)
+		card.z_index = 2
+	else:
+		card.scale = Vector2(1, 1)
+		card.z_index = 1
+	
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
@@ -27,7 +49,4 @@ func raycast_check_for_card():
 	if result:
 		return result[0].collider.get_parent()
 	return null
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+	
